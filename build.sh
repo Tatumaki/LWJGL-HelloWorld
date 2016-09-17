002d0e186aa6d7898f53a18128d78c8f1656cb61
Tatumaki <tatumaki.x.euphoric@gmail.com>
2016-09-18 02:30:21 +0900

    ビルド済みファイルの生成場所を変更

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-javac -classpath .:./src:$JAVA_LIB_PATH/lwjgl/jar/lwjgl.jar ./src/*.java ./src/org/joml/*.java
+javac -Xlint:none -nowarn -classpath .:./src:$JAVA_LIB_PATH/lwjgl/jar/lwjgl.jar ./src/*.java ./src/org/joml/*.java -d ./bin
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -classpath .:./src:$JAVA_LIB_PATH/lwjgl/jar/lwjgl.jar -Djava.library.path=.:$JAVA_LIB_PATH/lwjgl/native Main
+java -classpath .:./bin:$JAVA_LIB_PATH/lwjgl/jar/lwjgl.jar -Djava.library.path=.:$JAVA_LIB_PATH/lwjgl/native Main
